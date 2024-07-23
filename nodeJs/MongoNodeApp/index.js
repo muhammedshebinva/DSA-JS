4a63dafa406994eda954eabb1ce1b7e8bd212b62
twinksaji223@gmail.com <muhammedshebinva@gmail.com>
2024-07-23 18:53:44 +0530

    mongo db connection

--- nodeJs/MongoNodeApp/index.js
+++ nodeJs/MongoNodeApp/index.js
@@ -3,8 +3,45 @@ const mongoose = require('mongoose')
 
 
 const app = express()
+app.use(express.urlencoded({extended:false}))
+//connect mongodb
+mongoose.connect('mongodb://127.0.0.1:27017/my-students').then(()=>{
+    console.log('mongodb Connected')
+}).catch((err)=>{
+    console.log(err)
+})
+//schema
+const userSchema = mongoose.Schema({
+    firstName:{
+        type: String, 
+        require: true
+    },
+    lastName:{
+        type: String, 
+    },
+    email:{
+        type: String, 
+        require: true
+    }
+})
 
+//model
+const User = mongoose.model('app_users', userSchema)
 
+app.get('/',(req,res)=>{
+    res.send('mongo App Home')
+})
+
+app.post('/api/user',async (req,res)=>{
+    const body = req.body
+    const result = await User.create({
+        firstName:body.firstName,
+        lastName:body.lastName,
+        email:body.email
+    })
+    console.log('result', result)
+    return res.status(201).json({msg:'sucess'})
+});
 
 app.listen(3001,()=>{
     console.log('srever Started');
