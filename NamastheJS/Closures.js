dee29f2a3c16a170b93e279e0f865ef70d1dffae
twinksaji223@gmail.com <muhammedshebinva@gmail.com>
2024-09-05 11:20:28 +0530

    node and js code

---
+++ NamastheJS/Closures.js
@@ -0,0 +1,29 @@
+var a = 10
+console.log(a)
+
+function Counter(){
+    var count = 0;
+    this.increment= function(){
+        count ++
+        console.log(count)
+    }
+
+    this.decrement = function(){
+        count --;
+        console.log(count);
+    }
+}
+
+
+var counter1 = new Counter()
+
+counter1.increment()
+counter1.increment()
+counter1.increment()
+
+counter1.decrement()
+counter1.decrement()
+
+
+
+
